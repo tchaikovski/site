c8bcabd05538f9a19db76c486f80aef998acd426
Vladimir <tchaikovski@mail.ru>
2021-02-22 17:28:17 +0300

    Добавил создаем карту сайта

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit'
-
+    'django.contrib.sites'
+    'django.contrib.sitemaps'
 ]
 
 MIDDLEWARE = [
