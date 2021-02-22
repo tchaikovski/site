85cc3a04bc5d895be9dff03c926317f82b9b7847
Vladimir <tchaikovski@mail.ru>
2021-02-22 17:55:55 +0300

    Добавили RSS

--- blog/urls.py
+++ blog/urls.py
@@ -1,15 +1,14 @@
 from django.urls import path
 from . import views
-
+from .feeds import LastPostsFeed
 
 app_name = 'blog'
 urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
     # path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-         views.post_detail,
-         name='post_detail'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
+    path('feed/', LastPostsFeed(), name='post_feed'),
 ]
